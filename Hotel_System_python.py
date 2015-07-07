20202fc902a6525563d49b98a83b921402b4cb3b
MSeymenska <mihaela.seymenska@abv.bg>
2015-07-07 23:01:16 +0300

    created db

--- Hotel_System_python.py
+++ Hotel_System_python.py
@@ -1,7 +1,48 @@
-import cx_Oracle
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 #creates data base with the following attributes:(show the paper)
 #First name, Last name, EGN, Room type, Prize for a night,Prize for breakfast, Number of Accomodators, Check-in date, Check-out date, Breakfast, Children
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base = declarative_base()
+class ReservationDB(Base):
+	__tablename__ = 'reservation'
+	EGN = Column(String, primary_key = True)
+	numberRoom = Column(String, ForeignKey('Room.roomNumber'))
+	breakfastAdult = Column(String)
+	breakfastChild = Column(String)
+	dateCheckIn = Column(Date)
+	dateCheckOut = Column(Date)
+	prize = Column(Integer)
+	children = Column(String)
+
+	room = relationship("Room", backref = backref('reservation'))
+
+class Room(Base):
+	__tablename__ = 'room'
+	roomNumber = Column(String, primary_key = True)
+	roomType = Column(String)
+	prize = Column(Integer)
+
+	reservationRoomNumber = Column(String, ForeignKey('ReservationDB.numberRoom'))
+
+	reservation = relationship("ReservationDB", backref = backref('room'))
+
+class Person(Base):
+	__tablename__ = 'people'
+	firstName = Column(String)
+	lastName = Column(String)
+	personEGN = Column(String, primary_key = True)
+
+	reservationEGN = Column(String, ForeignKey('ReservationDB.EGN'))
+
+	reserve = relationship("ReservationDB", backref = backref('people'))
+
+
+
 
 class HotelReservation(object):
 	def __init__(self):
@@ -26,6 +67,7 @@ class HotelReservation(object):
 		#clients can check the availability by filtering with room_type, check-in_date, check-out_date
 
 	def __check(self):
+		pass
 		#makes verification for available rooms before confirming
 
 	def __refresh_db(self):
